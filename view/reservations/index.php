a5dcb7af23b166a42c3ea49abc7bea1a18b51d25
GijsHt <ghaithhatamleh@gmail.com>
2020-06-26 11:30:34 +0200

    styling een beetje aangepast

--- view/reservations/index.php
+++ view/reservations/index.php
@@ -12,7 +12,7 @@ $cities = getReservedCities();
         </ul>
     </div>
 </div>
-    <h3 class="text-light float-right mr-5 mt-2" id="helloUser">Hello <?= " ". $loggedInUser?></h3>
+    <h3 class="text-light float-right mr-5 mt-2 helloUser" >Hello <?= " ". $loggedInUser?></h3>
 
 <h1>Reservations you made</h1>
 
